4440247c36a94151146305320536bc800f8f4ea4
haechanmoon <timmymoon@naver.com>
2025-10-25 21:17:12 +0900

    feat(Validator) : 이름이 공백일 경우 검사

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -1,15 +1,20 @@
 package racingcar;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
 
     public static void validateCarNames(List<String> names) {
+        Set<String> onlyOneName = new HashSet<>();
         for (String name : names) {
             if (name.length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다. 구분자를 확인하세요");
+            }
+            if (name.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름이 공백일 수 없습니다.");
             }
         }
-
     }
 }
