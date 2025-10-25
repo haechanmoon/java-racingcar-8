9e8d95b58342b4069fd64d640aa397d240c53e28
haechanmoon <timmymoon@naver.com>
2025-10-25 13:26:36 +0900

    feat(Messages) : Messages 관리하는 class 생성

---
+++ src/main/java/racingcar/Messages.java
@@ -0,0 +1,6 @@
+package racingcar;
+
+public class Messages {
+    static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    static final String ASK_TURNS = "시도할 횟수는 몇 회인가요?";
+}
