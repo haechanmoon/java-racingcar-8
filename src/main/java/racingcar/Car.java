c91c1586a06e6c813c1219427c324c88593e042c
haechanmoon <timmymoon@naver.com>
2025-10-25 15:13:49 +0900

    feat(Car) : 0-9에서 무작위 값 추출 함수 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,19 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+    }
+
+}
