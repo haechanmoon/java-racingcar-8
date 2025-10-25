1b28cb71f66d2d2850db7554c253e746734277f0
haechanmoon <timmymoon@naver.com>
2025-10-25 17:53:00 +0900

    feat(Messages) : 실행 결과, 최종 우승자 Message 생성

--- src/main/java/racingcar/Messages.java
+++ src/main/java/racingcar/Messages.java
@@ -3,4 +3,6 @@ package racingcar;
 public class Messages {
     static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     static final String ASK_TURNS = "시도할 횟수는 몇 회인가요?";
+    static final String EXECUTION_RESULT = "실행 결과";
+    static final String TOTAL_RESULT = "최종 우승자";
 }
