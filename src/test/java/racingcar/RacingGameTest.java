b4e6889d191e09a77d86fadeb55d72b347c302cf
haechanmoon <timmymoon@naver.com>
2025-10-26 21:37:32 +0900

    test(RacingGameTest) : 공동 우승자를 올바르게 선별하는지 테스트

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -25,7 +25,20 @@ class RacingGameTest extends NsTest {
         );
     }
 
+    @Test
+    @DisplayName("공동 우승 테스트")
+    void 공동_우승_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
     @Override
     public void runMain() {
+        Application.main(new String[]{});
     }
 }
