0be3947d18d3cb01f5d3fd086625d08a2553ec6e
haechanmoon <timmymoon@naver.com>
2025-10-26 20:06:26 +0900

    test(CarTest) : 랜덤 숫자가 4 이상일 때, 자동차의 `position`이 1 증가하는지 테스트

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest extends NsTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @Test
+    @DisplayName("전진 테스트")
+    void 전진_테스트() {
+        Car car = new Car("pobi");
+        assertRandomNumberInRangeTest(
+                () -> car.move(), MOVING_FORWARD
+        );
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Override
+    public void runMain() {
+
+    }
+}
