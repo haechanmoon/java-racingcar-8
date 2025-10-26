652ef858358e82e57f01d1719c27a671331bf495
haechanmoon <timmymoon@naver.com>
2025-10-26 21:17:03 +0900

    test(RacingGameTest) : 단독 우승자를 올바르게 선별하는지 테스트

---
+++ src/test/java/racingcar/RacingGameTest.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class RacingGameTest extends NsTest {
+
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @Test
+    @DisplayName("단독 우승 테스트")
+    void 단독_우승_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Override
+    public void runMain() {
+    }
+}
