5b3b0196251ee3084f9a8c2b8bca168b72c8b71c
haechanmoon <timmymoon@naver.com>
2025-10-26 17:18:51 +0900

    feat(InputView) : 사용자가 잘못된 값 입력할 경우 IllegalArgumentException 발생. 어플리케이션 종료

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -1,18 +1,25 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
     public List<String> getCarNames() {
         System.out.println(Messages.ASK_CAR_NAMES);
-        String[] carNames = Console.readLine().split(",");
-        return List.of(carNames);
+        String[] carNamesArray = Console.readLine().split(",");
+        List<String> carNamesList = new ArrayList<>();
+        for (String name : carNamesArray) {
+            carNamesList.add(name.trim());
+        }
+
+        Validator.validateCarNames(carNamesList);
+        return carNamesList;
     }
 
     public int getTurns() {
         System.out.println(Messages.ASK_TURNS);
-        int turns = Integer.parseInt(Console.readLine());
-        return turns;
+        String input = Console.readLine();
+        return Validator.validateTurns(input);
     }
 }
