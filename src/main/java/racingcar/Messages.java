3f95a714b7952fcc64183e134d1e4b6cfd133200
haechanmoon <timmymoon@naver.com>
2025-12-05 17:02:32 +0900

    feat(Messages) : 상수 메세지 관리

---
+++ src/main/java/racingcar/Messages.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class Messages {
+    public static final String ASK_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String ASK_TURNS = "시도할 횟수는 몇 회인가요?";
+    public static final String RUN_RESULT = "실행 결과";
+    public static final String FINAL_CHAMPION = "최종 우승자 : ";
+}
