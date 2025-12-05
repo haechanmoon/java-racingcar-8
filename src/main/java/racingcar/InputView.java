23a5fba6120d8c1e31b036f2e9c95d3107a911f9
haechanmoon <timmymoon@naver.com>
2025-12-05 17:10:31 +0900

    feat(InputView) : 자동차 이름 입력 받기

---
+++ src/main/java/racingcar/InputView.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+
+    public String readInputNames() {
+        return Console.readLine();
+    }
+}
