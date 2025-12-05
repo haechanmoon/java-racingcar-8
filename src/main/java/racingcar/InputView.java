ac181e3198591ade126f24a6bb7a9f935b1f5c2b
haechanmoon <timmymoon@naver.com>
2025-12-05 17:16:05 +0900

    feat(InputView) : 시도할 횟수 입력 받기

--- src/main/java/racingcar/InputView.java
+++ src/main/java/racingcar/InputView.java
@@ -4,8 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-
     public String readInputNames() {
         return Console.readLine();
     }
+
+    public int readTurnCounts() {
+        return Integer.parseInt(Console.readLine());
+    }
+
 }
