0bfcf52025de519c9d990b2142227618db7ddb45
haechanmoon <timmymoon@naver.com>
2025-12-07 15:54:36 +0900

    chore(Messages) : Validator 에러메세지 추가

--- src/main/java/racingcar/Messages.java
+++ src/main/java/racingcar/Messages.java
@@ -5,4 +5,9 @@ public class Messages {
     public static final String ASK_TURN_COUNTS = "시도할 횟수는 몇 회인가요?";
     public static final String RUN_RESULT = "실행 결과";
     public static final String FINAL_CHAMPION = "최종 우승자 : ";
+
+    public static final String ERROR_EMPTY = "빈 값은 허용되지 않습니다.";
+    public static final String ERROR_OVER_NAME = "이름은 5자 이하로 설정해야합니다.";
+    public static final String ERROR_NAME_DUPLICATE = "이름이 중복되었습니다.";
+    public static final String ERROR_IS_NOT_NUMBER = "숫자만 입력가능합니다.";
 }
