a18d8b8da591f3c22a854458307735e602293266
haechanmoon <timmymoon@naver.com>
2025-12-09 00:03:09 +0900

    test(RacingCarTest) : 전진시 "-" 계산로직 확인

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RacingCarTest extends NsTest {
 
@@ -15,14 +17,30 @@ class RacingCarTest extends NsTest {
     @Test
     @DisplayName("자동차 전진 확인")
     void 자동차_전진_확인() {
-        //given
+        // given
         RacingCar car = new RacingCar("pobi");
 
-        //when
+        // when
         car.go(5);
 
-        //then
+        // then
         assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @DisplayName("전진시 - 확인")
+    @ValueSource(strings = {"h", "f"})
+    void 전진시_대시_확인() {
+        //given
+        RacingCar pobi = new RacingCar("pobi");
+        pobi.go(5);
+        pobi.go(5);
+
+        //when
+        String dash = pobi.getPositionDash().toString();
+
+        //then
+        assertThat(dash).isEqualTo("--");
     }
 
 
