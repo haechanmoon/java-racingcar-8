19a8c958acbbef49db4781a3110f7fdc73177184
haechanmoon <timmymoon@naver.com>
2025-12-12 23:23:35 +0900

    test(ValidatorTest) : 시도할 횟수 입력이 [0,1fe,aaa] 일때 예외처리 후 종료 확인

--- src/test/java/racingcar/utils/ValidatorTest.java
+++ src/test/java/racingcar/utils/ValidatorTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ValidatorTest {
 
@@ -21,16 +23,23 @@ class ValidatorTest {
 
     @Test
     @DisplayName("입력이 공백일때 예외처리 후 종료 확인")
-    void 입력이_공백일_때(){
+    void 입력이_공백일_때() {
         //given
         String input = "";
 
         //when,then
-        assertThatThrownBy(()-> Validator.validateIsEmpty(input))
+        assertThatThrownBy(() -> Validator.validateIsEmpty(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Messages.ERROR_IS_EMPTY);
     }
 
-    @Test
-    @DisplayName("")
+    @ParameterizedTest
+    @DisplayName("시도할 횟수 입력이 [0,1fe,aaa] 일때 예외처리 후 종료 확인")
+    @ValueSource(strings = {"0", "1fe", "aaa"})
+    void 시도할_횟수_입력이_1이상의_자연수가_아닐_때(String turns) {
+        assertThatThrownBy(() -> Validator.validateTurns(turns))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Messages.ERROR_IS_NOT_DIGIT);
+    }
+
 }
