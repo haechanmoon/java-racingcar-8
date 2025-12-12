633d88efe6f5ec9201a9622ffd69b3d767b74654
haechanmoon <timmymoon@naver.com>
2025-12-12 15:49:01 +0900

    feat : 자동차 이름 입력요구 메시지 출력

---
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -0,0 +1,12 @@
+package racingcar.controller;
+
+
+import racingcar.views.OutputView;
+
+public class RacingCarGame {
+
+    public void start() {
+        OutputView.requestCarNameMessage();
+
+    }
+}
---
+++ src/main/java/racingcar/views/OutputView.java
@@ -0,0 +1,9 @@
+package racingcar.views;
+
+import racingcar.utils.Messages;
+
+public class OutputView {
+    public static void requestCarNameMessage() {
+        System.out.println(Messages.ASK_CAR_NAME);
+    }
+}
