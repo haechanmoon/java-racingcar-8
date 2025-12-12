5baecfbbd5ffcfc4a6e5a54ac533fc69fca4c85e
haechanmoon <timmymoon@naver.com>
2025-12-12 17:02:29 +0900

    feat : 각 자동차 객체 분리된 이름으로 생성

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,16 +1,37 @@
 package racingcar.controller;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import racingcar.domain.RacingCar;
 import racingcar.views.InputView;
 import racingcar.views.OutputView;
 
 public class RacingCarGame {
+    private String input;
 
     public void start() {
         OutputView.requestCarNameMessage();
-        String input = InputView.getCarNames();
+        input = InputView.getCarNames();
         OutputView.requestTurnMessage();
         int turns = InputView.getTurns();
         OutputView.printRunResultMessage();
+
+    }
+
+    public List<String> getNames() {
+        String[] carNames = input.split("[,]");
+        return new ArrayList<>(Arrays.asList(carNames));
+    }
+
+    public List<RacingCar> generateCars() {
+        List<String> names = getNames();
+        List<RacingCar> cars = new ArrayList<>();
+        for (String name : names) {
+            RacingCar car = new RacingCar(name);
+            cars.add(car);
+        }
+        return cars;
     }
 }
---
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -0,0 +1,12 @@
+package racingcar.domain;
+
+public class RacingCar {
+
+    private int position;
+    private String name;
+
+    public RacingCar(String name) {
+        this.name = name;
+        position = 0;
+    }
+}
