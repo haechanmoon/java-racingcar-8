bb61e286388b48ce95ceda981a88193f3351ee68
haechanmoon <timmymoon@naver.com>
2025-12-12 17:19:36 +0900

    feat : 각 자동차 객체들 랜덤값 실행

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -1,12 +1,28 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class RacingCar {
 
+    private static final int MIN_RANDOM_NUM = 0;
+    private static final int MAX_RANDOM_NUM = 9;
+    private static final int STANDARD_NUM = 4;
     private int position;
-    private String name;
+    private final String name;
 
     public RacingCar(String name) {
         this.name = name;
         position = 0;
     }
+
+    public void go() {
+        if (STANDARD_NUM <= getRandomNum()) {
+            position++;
+        }
+    }
+
+    public int getRandomNum() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUM, MAX_RANDOM_NUM);
+    }
+
 }
